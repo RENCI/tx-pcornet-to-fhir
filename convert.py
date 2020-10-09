cfcc7a4ea1800eb8ce4f7a2be5fea3d5d9ff2055
hyi <hongyi@renci.org>
2020-10-09 17:55:13 -0400

    get condition conversion working

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 import sys
 import pandas as pd
 
-from utils import patient_conversion, medicationrequest_conversion, encounter_conversion
+from utils import patient_conversion, medicationrequest_conversion, encounter_conversion, condition_conversion
 
 
 def mapping_pcornet_to_fhir(input_path, output_path, partition):
@@ -10,6 +10,7 @@ def mapping_pcornet_to_fhir(input_path, output_path, partition):
     patient_conversion(input_path, map_df, output_path, partition)
     medicationrequest_conversion(input_path, map_df, output_path, partition)
     encounter_conversion(input_path, map_df, output_path, partition)
+    condition_conversion(input_path, map_df, output_path, partition)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -20,11 +20,15 @@ def write_fhir_json(content, input_path, input_file_name):
         json.dump(content, outfile, indent=4)
 
 
-def get_input_df(table_cd, input_path, map_df):
+def get_input_df(table_cd, input_path, map_df, use_cols=None):
     input_file_name = f'{table_cd}.csv'
     input_file = os.path.join(input_path, input_file_name)
     subset_map_df = map_df.loc[table_cd, :]
-    input_df = pd.read_csv(input_file, sep='|')
+
+    if use_cols:
+        input_df = pd.read_csv(input_file, sep='|', usecols=use_cols)
+    else:
+        input_df = pd.read_csv(input_file, sep='|')
     return input_df, subset_map_df
 
 
@@ -119,7 +123,7 @@ def medicationrequest_conversion(input_path, map_df, output_path, partition):
                 {
                   "text": prescribe_subset_map_df.loc['RX_FREQUENCY', row['RX_FREQUENCY']].at['fhir_out_cd'],
                   "asNeededBoolean": prescribe_subset_map_df.loc['RX_PRN_FLAG', row['RX_PRN_FLAG']].at['fhir_out_cd']
-                  if not pd.isnull(row['RX_PRN_FLAG']) else 'null',
+                  if not pd.isnull(row['RX_PRN_FLAG']) else None,
                   "route": {
                     "coding": [
                       {
@@ -240,10 +244,9 @@ def encounter_conversion(input_path, map_df, output_path, partition):
         return
 
     input_df, subset_map_df = get_input_df("ENCOUNTER", input_path, map_df)
-    dx_df = pd.read_csv(os.path.join(input_path, "DIAGNOSIS.csv"), sep='|')
-    filter_dx_df_col = dx_df.filter(items=['DIAGNOSISID', 'ENCOUNTERID', 'PDX', 'DX_SOURCE'])
-    filter_dx_df = filter_dx_df_col.loc[(filter_dx_df_col['PDX'] == 'P')
-                                        & (filter_dx_df_col['DX_SOURCE'] == 'DI')]
+    dx_df = pd.read_csv(os.path.join(input_path, "DIAGNOSIS.csv"), sep='|',
+                        usecols=['DIAGNOSISID', 'ENCOUNTERID', 'PDX', 'DX_SOURCE'])
+    filter_dx_df = dx_df.loc[(dx_df['PDX'] == 'P') & (dx_df['DX_SOURCE'] == 'DI')]
     join_df = pd.merge(input_df, filter_dx_df, how='left', left_on=['ENCOUNTERID'], right_on=['ENCOUNTERID'])
     i = 0
     partition = int(partition)
@@ -259,5 +262,144 @@ def encounter_conversion(input_path, map_df, output_path, partition):
         part_pat_df_len = len(part_pat_df)
         file_name = f'{part_pat_df_len}.json'
         write_fhir_json(bundle(entry=enc_fhir_entries), enc_dir, file_name)
+        i = i + partition
+    return
+
+
+def condition_conversion(input_path, map_df, output_path, partition):
+    def map_one_condition(row):
+        entry = {
+            "fullUrl": "https://www.hl7.org/fhir/condition.html",
+            "resource": {
+                "resourceType": "Condition",
+                "id": row['CON_IDENTIFIER'],
+                "clinicalStatus": row['CON_CLINSTATUS'],
+                "subject": {
+                    "reference": "Patient/{}".format(row['CON_SUBJECT_REFERENCE'])
+                },
+                "context": {
+                    "reference": "Encounter/{}".format(row['CON_CONTEXT_REFERENCE'])
+                }
+            }
+        }
+
+        if not pd.isnull(row['CON_CATEGORY_CODING_SYST']) or not pd.isnull(row['CON_CATEGORY_CODING_CODE']):
+            cat_dict = {}
+            code_dict = {}
+            if not pd.isnull(row['CON_CATEGORY_CODING_SYST']):
+                code_dict['system'] = row['CON_CATEGORY_CODING_SYST']
+            if not pd.isnull(row['CON_CATEGORY_CODING_CODE']):
+                code_dict['code'] = row['CON_CATEGORY_CODING_CODE']
+            cat_dict['coding'] = [code_dict]
+            entry['resource']['category'] = [cat_dict]
+
+        if not pd.isnull(row['CON_CODE_CODING_SYST']) or not pd.isnull(row['CON_CODE_CODING_CODE']):
+            coding_dict = {}
+            code_dict = {}
+            if not pd.isnull(row['CON_CODE_CODING_SYST']):
+                coding_dict['system'] = row['CON_CODE_CODING_SYST']
+            if not pd.isnull(row['CON_CODE_CODING_CODE']):
+                coding_dict['code'] = row['CON_CODE_CODING_CODE']
+            code_dict['coding'] = [coding_dict]
+            entry['resource']['code'] = code_dict
+
+        if not pd.isnull(row['CON_ASSERTER_REFERENCE']):
+            entry['resource']['asserter'] = {
+                "reference": "Practitioner/{}".format(row['CON_ASSERTER_REFERENCE'])
+            }
+
+        if not pd.isnull(row['CON_ASSERT_DATE']):
+            entry['resource']['assertedDate'] = row['CON_ASSERT_DATE']
+
+        if not pd.isnull(row['CON_ABATEDATE']):
+            entry['resource']['abatementDateTime'] = row['CON_ABATEDATE']
+
+        if not pd.isnull(row['CON_ONSETDATE']):
+            entry['resource']['onsetDateTime'] = row['CON_ONSETDATE']
+
+        cond_fhir_entries.append(entry)
+        return
+
+    filter_cols = ["CONDITIONID", "PATID", "ENCOUNTERID", "CONDITION_TYPE", "CONDITION", "REPORT_DATE",
+                    "RESOLVE_DATE", "ONSET_DATE", 'CONDITION_STATUS', 'CONDITION_SOURCE']
+    input_df, subset_map_df = get_input_df("CONDITION", input_path, map_df, use_cols=filter_cols)
+    input_df = input_df.loc[input_df['CONDITION_SOURCE'] == 'HC']
+    input_df.drop(columns=["CONDITION_SOURCE"])
+    input_df.loc[(input_df.CONDITION_TYPE != 'HP') &
+                 (input_df.CONDITION_TYPE != 'SM') &
+                 (input_df.CONDITION_TYPE != '09') &
+                 (input_df.CONDITION_TYPE != '10') &
+                 (input_df.CONDITION_TYPE != '11'), 'CONDITION_TYPE'] = None
+    input_df.loc[(input_df.CONDITION_TYPE == '09'), 'CONDITION_TYPE'] = 'http://hl7.org/fhir/sid/icd-9-cm'
+    input_df.loc[(input_df.CONDITION_TYPE == '10'), 'CONDITION_TYPE'] = 'http://hl7.org/fhir/sid/icd-10-cm'
+    input_df.loc[(input_df.CONDITION_TYPE == '11'), 'CONDITION_TYPE'] = 'http://hl7.org/fhir/sid/icd-11-cm'
+    input_df.loc[(input_df.CONDITION_TYPE == 'SM'), 'CONDITION_TYPE'] = 'http://snomed.info/sct'
+    input_df.loc[(input_df.CONDITION_TYPE == 'HP'), 'CONDITION_TYPE'] = 'https://hpo.jax.org/app/'
+    input_df['CONDITION_STATUS'] = input_df['CONDITION_STATUS'].map(
+        lambda x: subset_map_df.loc['CONDITION_STATUS', x].at['fhir_out_cd'])
+    mapping = {"CONDITIONID": "CON_IDENTIFIER",
+               "PATID": "CON_SUBJECT_REFERENCE",
+               "ENCOUNTERID": "CON_CONTEXT_REFERENCE",
+               "CONDITION_TYPE": "CON_CODE_CODING_SYST",
+               "CONDITION": "CON_CODE_CODING_CODE",
+               "REPORT_DATE": "CON_ASSERT_DATE",
+               "RESOLVE_DATE": "CON_ABATEDATE",
+               "ONSET_DATE": "CON_ONSETDATE",
+               "CONDITION_STATUS": "CON_CLINSTATUS"}
+    input_df.rename(columns=mapping, inplace=True)
+    input_df['CON_ASSERTER_REFERENCE'] = None
+    input_df['CON_CATEGORY_CODING_SYST'] = 'https://www.hl7.org/fhir/valueset-condition-category'
+    input_df['CON_CATEGORY_CODING_CODE'] = 'problem-list-item'
+    input_df = input_df.drop_duplicates()
+
+    dx_df = pd.read_csv(os.path.join(input_path, "DIAGNOSIS.csv"), sep='|',
+                        usecols=['DIAGNOSISID', 'ENCOUNTERID', 'PATID', 'PROVIDERID', 'DX_TYPE',
+                                 'DX', 'ADMIT_DATE'])
+    dx_df.loc[(dx_df.DX_TYPE != 'SM') &
+              (dx_df.DX_TYPE != '09') &
+              (dx_df.DX_TYPE != '10') &
+              (dx_df.DX_TYPE != '11'), 'DX_TYPE'] = None
+    dx_df.loc[(dx_df.DX_TYPE == '09'), 'DX_TYPE'] = 'http://hl7.org/fhir/sid/icd-9-cm'
+    dx_df.loc[(dx_df.DX_TYPE == '10'), 'DX_TYPE'] = 'http://hl7.org/fhir/sid/icd-10-cm'
+    dx_df.loc[(dx_df.DX_TYPE == '11'), 'DX_TYPE'] = 'http://hl7.org/fhir/sid/icd-11-cm'
+    dx_df.loc[(dx_df.DX_TYPE == 'SM'), 'DX_TYPE'] = 'http://snomed.info/sct'
+    mapping = {"DIAGNOSISID": "CON_IDENTIFIER",
+               "PATID": "CON_SUBJECT_REFERENCE",
+               "ENCOUNTERID": "CON_CONTEXT_REFERENCE",
+               "PROVIDERID": "CON_ASSERTER_REFERENCE",
+               "DX_TYPE": "CON_CODE_CODING_SYST",
+               "DX": "CON_CODE_CODING_CODE",
+               "ADMIT_DATE": "CON_ASSERT_DATE"}
+    dx_df.rename(columns=mapping, inplace=True)
+    dx_df['CON_CATEGORY_CODING_SYST'] = 'https://www.hl7.org/fhir/valueset-condition-category'
+    dx_df['CON_CATEGORY_CODING_CODE'] = 'encounter-diagnosis'
+    dx_df['CON_CLINSTATUS'] = 'active'
+    dx_df['CON_ABATEDATE'] = None
+    dx_df['CON_ONSETDATE'] = None
+    dx_df = dx_df.drop_duplicates()
+    join_df = pd.concat([dx_df, input_df])
+
+    i = 0
+    partition = int(partition)
+    join_df_len = len(join_df)
+
+    cond_dir = os.path.join(output_path, 'Condition')
+
+    while i < join_df_len:
+        cond_fhir_entries = []
+        file_name = f'{i}.json'
+        write_fhir_json(bundle(), cond_dir, file_name)
+        if i + partition >= join_df_len:
+            if i == 0:
+                part_pat_df = join_df
+            else:
+                part_pat_df = join_df.loc[i:i + join_df_len, :]
+        else:
+            part_pat_df = join_df.loc[i:i + partition, :]
+
+        part_pat_df.apply(lambda row: map_one_condition(row), axis=1)
+        part_pat_df_len = len(part_pat_df)
+        file_name = f'{part_pat_df_len}.json'
+        write_fhir_json(bundle(entry=cond_fhir_entries), cond_dir, file_name)
         i = i + partition
     return
